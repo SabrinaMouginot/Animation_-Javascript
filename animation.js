bf7acbe94a7284332dd3fad930c1bea4b7ef2ebf
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-01-30 00:49:39 +0100

    commentaires d'explication sur la fonction et le requestAnimationFrame

--- animation.js
+++ animation.js
@@ -4,9 +4,14 @@ let i = 0;
 function hautBas(){
     console.log(i);
     i++;
+    requestAnimationFrame(hautBas);
 }
 
 // J'appelle une animation. On appelle en Call Back la fonction hautBas.
 requestAnimationFrame(hautBas);
 // Dans la console, on obtient un 0. L'animation a été lancée une fois. 
 //Si je rappelle i, on obtiendra le 1 parce que c'est i++.
+
+// En rajoutant un deuxième  requestAnimationFrame(hautBas); dans la fonction.
+// Je lance requestAnimationFrame extérieur à la fonction. J'appelle le hautBas qui effectue ce qu'on trouve dans la fonction. 
+// Et quand il arrive à la fin, il se rappelle lui-même avec requestAnimationFrame.
